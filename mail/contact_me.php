9e2905a2988de69825de8ab0469af6d50a267320
Nilay Jayswal <nilayjayswalnj@gmail.com>
2015-06-08 19:32:32 +0530

    commit
    update

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,7 +19,7 @@ $message = $_POST['message'];
 $to = 'nilayjayswalnj@gmail.com'; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; 
+$headers = "From: seasondesigns29@gmail.com"; 
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
