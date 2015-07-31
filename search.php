dbd2b0be23f9631d5374ab491a8025ffcb4ceb2b
Nilay Jayswal <nilayjayswalnj@gmail.com>
2015-07-31 19:03:51 +0530

    commit
    commit

---
+++ search.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">  
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">    
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="search/search.js"></script>
+ 	<script type="text/javascript" src="js/superfish.js"></script>
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>	
+	<script>		
+   jQuery(window).load(function() {	
+    jQuery('.spinner').animate({'opacity':0},1000,'easeOutCubic',function (){jQuery(this).css('display','none')});	
+   });			
+	</script>
+
+</head>
+
+<body class="bg-content">
+<div class="spinner"></div>
+<section id="content">
+  <div class="container">
+    <div class="row">
+    	<div class="col-lg-12">
+            <h3>Search result:</h3>
+            <div id="search-results"></div>
+		</div>  
+    </div>        
+</div>  
+</section>
+</div>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
