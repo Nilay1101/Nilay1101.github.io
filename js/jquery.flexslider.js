840a419d646a6cdf56f4a1655691b07c742f5220
Nilay Jayswal <nilayjayswalnj@gmail.com>
2016-05-08 17:24:00 +0530

    speed test 3

--- js/jquery.flexslider.js
+++ js/jquery.flexslider.js
@@ -1055,7 +1055,7 @@
     startAt: 0,                     //Integer: The slide that the slider should start on. Array notation (0 = first slide)
     slideshow: true,                //Boolean: Animate slider automatically
     slideshowSpeed: 300,           //Integer: Set the speed of the slideshow cycling, in milliseconds
-    animationSpeed: 300,            //Integer: Set the speed of animations, in milliseconds
+    animationSpeed: 100,            //Integer: Set the speed of animations, in milliseconds
     initDelay: 0,                   //{NEW} Integer: Set an initialization delay, in milliseconds
     randomize: false,               //Boolean: Randomize slide order
     thumbCaptions: false,           //Boolean: Whether or not to put captions on thumbnails when using the "thumbnails" controlNav.
